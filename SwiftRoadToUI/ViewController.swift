b802dbec6bf1b3a1b9653e3a5582dfbb25d10fdc
LShawn <730356947@qq.com>
2015-05-16 13:10:33 +0800

    11

--- SwiftRoadToUI/ViewController.swift
+++ SwiftRoadToUI/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        //卧槽
         // Dispose of any resources that can be recreated.
     }
 
